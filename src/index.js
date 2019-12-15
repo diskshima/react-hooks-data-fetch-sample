f4994c15d6d9db754613d69393fd2b8eb4c23f44
Daisuke Shimamoto <diskshima@gmail.com>
2019-12-15 18:58:08 +0900

    Add loading indicator

--- src/index.js
+++ src/index.js
@@ -10,12 +10,15 @@ function App() {
   const [url, setUrl] = useState(
     "https://hn.algolia.com/api/v1/search?query=react native"
   );
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       const result = await axios(url);
 
       setData(result.data);
+      setIsLoading(false);
     };
 
     fetchData();
@@ -36,13 +39,17 @@ function App() {
       >
         Search
       </button>
-      <ul>
-        {data.hits.map(item => (
-          <li key={item.objectID}>
-            <a href={item.url}>{item.title}</a>
-          </li>
-        ))}
-      </ul>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {data.hits.map(item => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
     </Fragment>
   );
 }
