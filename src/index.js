aa610ad489900515b0a38296759be000aeceb6ad
Daisuke Shimamoto <diskshima@gmail.com>
2019-12-15 18:23:09 +0900

    Accept queries from text box

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 
@@ -6,27 +6,35 @@ import "./styles.css";
 
 function App() {
   const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState("react native");
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
-        "https://hn.algolia.com/api/v1/search?query=react native"
+        `https://hn.algolia.com/api/v1/search?query=${query}`
       );
 
       setData(result.data);
     };
 
     fetchData();
-  }, []);
+  }, [query]);
 
   return (
-    <ul>
-      {data.hits.map(item => (
-        <li key={item.objectID}>
-          <a href={item.url}>{item.title}</a>
-        </li>
-      ))}
-    </ul>
+    <Fragment>
+      <input
+        type="text"
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+      />
+      <ul>
+        {data.hits.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
   );
 }
 
